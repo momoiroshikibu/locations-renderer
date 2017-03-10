83ca26bdd791bb41d33617d38cb3aac1eaf45ae6
momoiroshikibu <momoiroshikibu@gmail.com>
2017-03-10 23:52:12 +0900

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + '/src',
+    entry: {
+        app: './app.js'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js'
+    }
+};
